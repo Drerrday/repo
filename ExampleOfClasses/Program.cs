4218eb3aa5b73c4d01a13c7637ec6599e6f33c5b
Hunter Elam <hunterelam21@gmail.com>
2019-09-16 21:56:45 -0500

    working with products one

---
+++ ExampleOfClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExampleOfClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product prod = new Product();
+            prod.Name = "Nintendo Switch";
+            prod.Price = 299.99;
+
+            Console.WriteLine($"The Products name is {prod.Name} and it currently costs {prod.Price}.");
+        }
+    }
+}
